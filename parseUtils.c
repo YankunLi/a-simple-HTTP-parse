c4219f2b8358e023946e3d3a0bb54a92b1601ae6
lioveni <lioveni@users.noreply.github.com>
2014-12-04 10:57:56 +0800

    Update parseUtils.c

--- parseUtils.c
+++ parseUtils.c
@@ -8,3 +8,155 @@
 #include <fcntl.h>
 
 #include <parseUtils.h>
+
+
+LINE_STATUS parse_line( char* buffer, int& checked_index, int& read_index )
+{
+    char temp;
+    for ( ; checked_index < read_index; ++checked_index )
+    {
+        temp = buffer[ checked_index ];
+        if ( temp == '\r' )
+        {
+            if ( ( checked_index + 1 ) == read_index )
+            {
+                return LINE_OPEN;
+            }
+            else if ( buffer[ checked_index + 1 ] == '\n' )
+            {
+                buffer[ checked_index++ ] = '\0';
+                buffer[ checked_index++ ] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }
+        else if( temp == '\n' )
+        {
+            if( ( checked_index > 1 ) &&  buffer[ checked_index - 1 ] == '\r' )
+            {
+                buffer[ checked_index-1 ] = '\0';
+                buffer[ checked_index++ ] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }
+    }
+    return LINE_OPEN;
+}
+
+HTTP_CODE parse_requestline( char* szTemp, CHECK_STATE& checkstate )
+{
+    char* szURL = strpbrk( szTemp, " \t" );
+    if ( ! szURL )
+    {
+        return BAD_REQUEST;
+    }
+    *szURL++ = '\0';
+
+    char* szMethod = szTemp;
+    if ( strcasecmp( szMethod, "GET" ) == 0 )
+    {
+        printf( "The request method is GET\n" );
+    }
+    else
+    {
+        return BAD_REQUEST;
+    }
+
+    szURL += strspn( szURL, " \t" );
+    char* szVersion = strpbrk( szURL, " \t" );
+    if ( ! szVersion )
+    {
+        return BAD_REQUEST;
+    }
+    *szVersion++ = '\0';
+    szVersion += strspn( szVersion, " \t" );
+    if ( strcasecmp( szVersion, "HTTP/1.1" ) != 0 )
+    {
+        return BAD_REQUEST;
+    }
+
+    if ( strncasecmp( szURL, "http://", 7 ) == 0 )
+    {
+        szURL += 7;
+        szURL = strchr( szURL, '/' );
+    }
+
+    if ( ! szURL || szURL[ 0 ] != '/' )
+    {
+        return BAD_REQUEST;
+    }
+
+    //URLDecode( szURL );
+    printf( "The request URL is: %s\n", szURL );
+    checkstate = CHECK_STATE_HEADER;
+    return NO_REQUEST;
+}
+
+HTTP_CODE parse_headers( char* szTemp )
+{
+    if ( szTemp[ 0 ] == '\0' )
+    {
+        return GET_REQUEST;
+    }
+    else if ( strncasecmp( szTemp, "Host:", 5 ) == 0 )
+    {
+        szTemp += 5;
+        szTemp += strspn( szTemp, " \t" );
+        printf( "the request host is: %s\n", szTemp );
+    }
+    else
+    {
+        printf( "I can not handle this header\n" );
+    }
+
+    return NO_REQUEST;
+}
+
+HTTP_CODE parse_content( char* buffer, int& checked_index, CHECK_STATE& checkstate, int& read_index, int& start_line )
+{
+    LINE_STATUS linestatus = LINE_OK;
+    HTTP_CODE retcode = NO_REQUEST;
+    while( ( linestatus = parse_line( buffer, checked_index, read_index ) ) == LINE_OK )
+    {
+        char* szTemp = buffer + start_line;
+        start_line = checked_index;
+        switch ( checkstate )
+        {
+            case CHECK_STATE_REQUESTLINE:
+            {
+                retcode = parse_requestline( szTemp, checkstate );
+                if ( retcode == BAD_REQUEST )
+                {
+                    return BAD_REQUEST;
+                }
+                break;
+            }
+            case CHECK_STATE_HEADER:
+            {
+                retcode = parse_headers( szTemp );
+                if ( retcode == BAD_REQUEST )
+                {
+                    return BAD_REQUEST;
+                }
+                else if ( retcode == GET_REQUEST )
+                {
+                    return GET_REQUEST;
+                }
+                break;
+            }
+            default:
+            {
+                return INTERNAL_ERROR;
+            }
+        }
+    }
+    if( linestatus == LINE_OPEN )
+    {
+        return NO_REQUEST;
+    }
+    else
+    {
+        return BAD_REQUEST;
+    }
+}
