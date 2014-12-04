1383bf753c5667c420a9d13cc35715526070f26c
lioveni <lioveni@users.noreply.github.com>
2014-12-04 11:01:08 +0800

    Update parseUtils.h

--- parseUtils.h
+++ parseUtils.h
@@ -7,11 +7,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum CHECK_STATE { CHETC_STATE_REQUESTLINE = 0, CHECK_STATE_HEADER };
+#define BUFFER_SIZE 4096
+
+enum CHECK_STATE { CHECK_STATE_REQUESTLINE = 0, CHECK_STATE_HEADER, CHECK_STATE_CONTENT };
 
 enum LINE_STATUS { LINE_OK = 0, LINE_BAD, LINE_OPEN };
 
 enum HTTP_CODE { NO_REQUEST, GET_REQUEST, BAD_REQUEST, FORBIDDEN_REQUEST, INTERNAL_ERROR, CLOSED_CONNECTION };
+
+static const char* szret[] = { "I get a correct result\n", "Something wrong\n" };
 
 /* 从状态机，用于分析一行的内容 */
 lINE_STATUS parse_line( char* buffer, int& checked_index, int& read_index );
